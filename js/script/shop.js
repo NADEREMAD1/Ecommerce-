50b591ee7ddf0ddff552ab33cfa7b6b24eaa416c
NaderEmad <naderemad277@gmail.com>
2023-08-20 01:01:55 +0300

    20 / 8

--- js/script/shop.js
+++ js/script/shop.js
@@ -3,7 +3,6 @@ let myReq = new XMLHttpRequest();
 myReq.open("GET", "https://fakestoreapi.com/products");
 myReq.responseType = "json";
 myReq.send();
-
 myReq.onload = function () {
   let Posts = myReq.response;
   let den = document.getElementById("den");
@@ -12,37 +11,26 @@ myReq.onload = function () {
     den.innerHTML = "";
     for (Post of products) {
       den.innerHTML += `
-        <div class="product-container col">
-          <div class="content-Products ">
-            <img class="product-image " src="${Post["image"]}">
-            <h5 class="product-title ">${Post["title"]}</h5>
-            <h5 class="product-price ">${Post["price"]}</h5>
-        <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
-        <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
-        <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
-        <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
-         </div>
-          <button onclick="addToCart()" class="add-to-cart-button">Add to Cart</button>
+      <div class="product-container col" data-aos="zoom-in">
+        <div class="content-Products ">
+          <img class="product-image " src="${Post["image"]}">
+          <h5 class="product-title ">${Post["title"]}</h5>
+          <h5 class="product-price ">${Post["price"]}</h5>
+          <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
+          <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
+          <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
+          <i class="fa-regular fa-star-half-stroke" style="color: #10b5cb;"></i>
         </div>
+        <button onclick="addToCart('${Post.title}')" class="add-to-cart-button">Add to Cart</button>
+      </div>
       `;
     }
     addToCart();
   }
   displayProducts(Posts);
-  // Function to handle search
-  function searchProducts(searchTerm) {
-    let filteredProducts = Posts.filter((post) =>
-      post["title"].toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    displayProducts(filteredProducts);
-  }
-  // Event listener for form submission
-  document.querySelector("form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    let searchTerm = document.getElementById("searchInput").value;
-    searchProducts(searchTerm);
-  });
 };
+// ============================================
+
 // Start Function counter cart
 let cartCount = 0; // العدد الحالي للمنتجات في السلة
 function addToCart() {
@@ -55,8 +43,8 @@ function updateCartCount() {
   let cartElement = document.getElementById("cartCount");
   cartElement.innerText = cartCount; // تحديث قيمة العدد في العنصر المعرف بـ "cartCount"
 }
-
 // =======================================
+
 // Start Section Filter
 filterSelection("all");
 function filterSelection(c) {
@@ -91,16 +79,4 @@ function w3RemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-
 // End Section Filter
